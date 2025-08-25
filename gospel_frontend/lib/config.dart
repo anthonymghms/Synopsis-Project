ec5d91b496e368e75448f471b92270dd1ff2a4a8
Blizzard-Of-Oz <137295217+Blizzard-Of-Oz@users.noreply.github.com>
2025-08-25 16:00:17 +0300

    chore: point frontend to production backend URL

--- gospel_frontend/lib/config.dart
+++ gospel_frontend/lib/config.dart
@@ -1,7 +1,7 @@
 // Configuration for the Gospel Topics app
 
 // Base URL for the backend API.
-const apiBaseUrl = "http://192.168.20.183:5000";
+const apiBaseUrl = "http://164.68.108.181:8000";
 
 // Supported versions for each language. Update this map to expose
 // additional language/version combinations in the UI.
