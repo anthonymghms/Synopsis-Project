87115ea45eb7a7486a06331d381294070a3f1d12
kali <kali@kali.contaboserver.net>
2025-09-10 09:54:26 +0200

    Updated Flask service and Flutter setup

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def get_topics():
     return Response(json.dumps(topics, ensure_ascii=False, indent=2), content_type="application/json; charset=utf-8")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
